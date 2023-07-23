b7dd3491046f62d1e41784e4d9f9ff5be18ab91b
XFSZ <oncwnuP6BE0yBE71vLowXmxZ_uS8@git.weixin.qq.com>
2023-07-23 15:09:56 +0800

    创建swift包

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "MyClashLibrary",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "MyClashLibrary",
+            targets: ["Libclash"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibrary",
-            dependencies: []),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+//        .target(
+//            name: "MyLibrary",
+//            dependencies: []),
+//        .testTarget(
+//            name: "MyLibraryTests",
+//            dependencies: ["MyLibrary"]),
+        .binaryTarget(name: "Libclash", path: "./Sources/libclash.xcframework")
     ]
 )
